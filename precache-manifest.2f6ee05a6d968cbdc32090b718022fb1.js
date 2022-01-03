a53662ae4464358db7a32ecc1b74eeeb7ac2cfc9
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-03 19:04:54 -0300

    Updates

---
+++ precache-manifest.2f6ee05a6d968cbdc32090b718022fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3ef3c0041f1228b0c8c77c818293d2",
+    "url": "/trybe-project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "ddd067501dec13000929",
+    "url": "/trybe-project-movie-cards-library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/trybe-project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "ddd067501dec13000929",
+    "url": "/trybe-project-movie-cards-library/static/js/main.594e7d02.chunk.js"
+  },
+  {
+    "revision": "da432151a4c4f2487304",
+    "url": "/trybe-project-movie-cards-library/static/js/runtime-main.5891c939.js"
+  }
+]);
